4741e7e815202027b6a541f325ca1473ac7734c1
Travis Harrington <travis.harrington@gmail.com>
2023-01-09 15:28:11 -0700

    Annoyed and dont want to fix it ATM hardcoding tmux.

--- users/hex/home-manager.nix
+++ users/hex/home-manager.nix
@@ -24,6 +24,7 @@ in {
     pkgs.rustc
     pkgs.zip
     pkgs.newsboat
+    pkgs.tmux
   ];
 
   home.sessionVariables = {
@@ -38,6 +39,8 @@ in {
   programs.neovim = {
     enable = true;
   };
+  
+  home.file.".tmux.conf".source = ./.tmux.conf;
 
   xdg.configFile.nvim = {
     source    = ./nvim;
@@ -67,53 +70,6 @@ in {
       color.ui           = true;
       init.defaultBranch = "main";
     };
-  };
-
-  programs.tmux = {
-    enable = true;
-    keyMode = "vi";
-    terminal = "screen-256color";
-    extraConfig = ''
-    set -g prefix C-a
-    unbind C-b
-    bind-key C-a send-prefix
-
-    unbind %
-    bind | split-window -h
-
-    unbind '"'
-    bind - split-window -v
-
-    unbind r
-    bind r source-file ~/.tmux.conf
-
-    bind -r j resize-pane -D 5
-    bind -r k resize-pane -U 5
-    bind -r l resize-pane -R 5
-    bind -r h resize-pane -L 5
-    
-    bind -r m resize-pane -Z
-    
-    set -g mouse on
-    
-    bind-key -T copy-mode-vi 'v' send -X begin-selection
-    bind-key -T copy-mode-vi 'y' send -X copy-selection
-    
-    unbind -T copy-mode-vi MouseDragEnd1Pane
-    
-    set -g @plugin 'tmux-plugins/tpm'
-    set -g @plugin 'christoomey/vim-tmux-navigator'
-    set -g @plugin 'jimeh/tmux-themepack'
-    set -g @plugin 'tmux-plugins/tmux-resurrect'
-    set -g @plugin 'tmux-plugins/tmux-continuum'
-    
-    set -g @themepack 'powerline/default/cyan'
-    
-    set -g @resurrect-capture-pane-contents 'on'
-    set -g @continuum 'on'
-    
-    run '~/.tmux/plugins/tpm/tpm'
-    '';
   };
 
   programs.kitty = {
